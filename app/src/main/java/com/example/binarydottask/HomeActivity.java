cc9531e07c8956426a11c2d1fabca53e85e8bcc8
vishal R <vishal206ani@gmail.com>
2021-12-09 22:02:16 +0530

    API connected & data retrieved.

--- app/src/main/java/com/example/binarydottask/HomeActivity.java
+++ app/src/main/java/com/example/binarydottask/HomeActivity.java
@@ -6,9 +6,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.binarydottask.adaptor.userAdaptor;
 import com.example.binarydottask.model.users;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -37,7 +47,52 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void setUserInfo() {
-        usersArrayList.add(new users("vishal","ravi","vishalravi123@gmail.com","1",""));
-        usersArrayList.add(new users("vishwesh","ravi","vishalrav345@gmail.com","2",""));
+//        usersArrayList.add(new users("vishal","ravi","vishalravi123@gmail.com","1",""));
+//        usersArrayList.add(new users("vishwesh","ravi","vishalrav345@gmail.com","2",""));
+
+        String url = "https://reqres.in/api/users?page=2";
+
+
+        StringRequest request
+                = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(
+                            String response)
+                    {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response.toString());
+
+                            int n=jsonObject.getJSONArray("data").length();
+                            System.out.println("--"+n);
+                            for (int i = 0; i<n; i++) {
+                                System.out.println("+");
+                                String firstName=jsonObject.getJSONArray("data").getJSONObject(i).getString("first_name").toString();
+                                String secondName=jsonObject.getJSONArray("data").getJSONObject(i).getString("last_name").toString();
+                                String email=jsonObject.getJSONArray("data").getJSONObject(i).getString("email").toString();
+                                String id=jsonObject.getJSONArray("data").getJSONObject(i).getString("id").toString();
+                                String image=jsonObject.getJSONArray("data").getJSONObject(i).getString("avatar").toString();
+                                usersArrayList.add(new users(firstName,secondName,email,id,image));
+                            }
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(
+                            VolleyError error)
+                    {
+                        Toast.makeText(HomeActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        RequestQueue requestQueue
+                = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
--- app/src/main/java/com/example/binarydottask/adaptor/userAdaptor.java
+++ app/src/main/java/com/example/binarydottask/adaptor/userAdaptor.java
@@ -4,9 +4,12 @@
 
 package com.example.binarydottask.adaptor;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.binarydottask.R;
 import com.example.binarydottask.model.users;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class userAdaptor extends RecyclerView.Adapter<userAdaptor.MyViewHolder> {
@@ -37,6 +44,28 @@ public class userAdaptor extends RecyclerView.Adapter<userAdaptor.MyViewHolder> 
         String uName=usersArrayList.get(position).getFirstName()+" "+usersArrayList.get(position).getSecondName();
         String uEmail=usersArrayList.get(position).getEmail();
         String uId="ID: "+usersArrayList.get(position).getId();
+        String url=usersArrayList.get(position).getImage();
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL(url).getContent());
+                        holder.uImage.setImageBitmap(bitmap);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
         holder.uName.setText(uName);
         holder.uEmail.setText(uEmail);
         holder.uId.setText(uId);
@@ -49,11 +78,13 @@ public class userAdaptor extends RecyclerView.Adapter<userAdaptor.MyViewHolder> 
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView uName,uEmail,uId;
+        private ImageView uImage;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             uName=itemView.findViewById(R.id.text_userName);
             uEmail=itemView.findViewById(R.id.text_userEmail);
             uId=itemView.findViewById(R.id.text_userId);
+            uImage=itemView.findViewById(R.id.Image_user);
         }
     }
 }
